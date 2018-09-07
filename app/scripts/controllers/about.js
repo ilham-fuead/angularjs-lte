62fbb2d4a251fe0aa8351f71a98200509c426da5
Mohd Ilhammuddin Bin Mohd Fuead <ilham.fuead@gmail.com>
2018-09-07 10:45:52 +0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeorndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yeorndApp
+ */
+angular.module('yeorndApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeorndApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeorndApp
+ */
+angular.module('yeorndApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
